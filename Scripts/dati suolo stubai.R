044ab94f088f40a212da0c6766164371f4f9b78f
Sebastianmarzini <sebastianmarzini@gmail.com>
2022-07-04 09:27:00 +0200

    spostamento files

--- Scripts/dati suolo stubai.R
+++ Scripts/dati suolo stubai.R
@@ -12,23 +12,19 @@ library(RSQLite)
 library(tidyverse)
 library(stringr)
 
-x <- raster("Dati aree studio/Stubai/STUBAI/Georg/Soil_data_STUBAI/Soil_data_Stubai.img")
+# img con coordinate e colonna con collegamento a file xlsx contenente i dati per tessitura, carbonio e azoto
+soil_param <- raster("Dati aree studio/Stubai/Georg/Soil_data_STUBAI/Soil_data_Stubai.img")
+stc <- as.data.frame(soil_param, xy=TRUE) %>% rename (Trajectory = Soil_data_STUBAI)
+# file xlsx con dati vari suolo
+soildata <- as.data.frame(readxl::read_xlsx("Dati aree studio/Stubai/Georg/C_N_Texture_FIN.xlsx"))
+sdf <- left_join(stc, soildata, by = "Trajectory")
 
-plot(x)
+# tif con dati profondità suolo
+soil_depth <- raster("Dati aree studio/Stubai/Georg/Soil_data_STUBAI/Soil_depth.tif") 
+sd <- as.data.frame(rasterToPoints(soil_depth))
 
-df <- as.data.frame(x, xy=TRUE) %>% rename (Trajectory = Soil_data_STUBAI)
-
-soildata <- as.data.frame(readxl::read_xlsx("Dati aree studio/Stubai/STUBAI/Georg/C_N_Texture_FIN.xlsx"))
-
-df <- left_join(df, soildata, by = "Trajectory")
-
-soil_depth <- raster("Dati aree studio/Stubai/STUBAI/Georg/Soil_data_STUBAI/Soil_depth.tif") ## problema crs
-
-sd <- rasterToPoints(soil_depth)
-sd_df <- as.data.frame(sd)
-
-df$xy <- paste(df$x, df$y, sep="")
-sd_df$xy <- paste(sd_df$x, sd_df$y, sep="")
+sdf$xy <- paste(df$x, df$y, sep="")
+sd$xy <- paste(sd$x, sd$y, sep="")
 
 df <- left_join(df, sd_df, by = "xy")
 
